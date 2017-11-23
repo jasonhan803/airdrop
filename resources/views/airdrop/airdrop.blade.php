163f28bd7b85ac582e601128de9035c303661d37
Raj Kumar <rajkumar@lalaworld.io>
2017-11-23 19:10:28 +0530

    Mail Airdrop Register

--- resources/views/airdrop/airdrop.blade.php
+++ resources/views/airdrop/airdrop.blade.php
@@ -70,6 +70,7 @@
         </div>
         <div class="col-12 col-md-6 col-lg-4 task">
           <div class="card card-bordered card-hover-shadow text-center">
+            <a href="javascript:fbShare('Fb Share', 'Facebook share popup', 'http://goo.gl/dS52U', 520, 350)">
             <span id="share" class="card-block">
               <p>
                 <i class="fa fa-facebook fs-50 text-muted" style="color:#00a99d !important;"></i>
@@ -81,6 +82,7 @@
                 Post
               </button-->
             </span>
+          </a>
           </div>
         </div>
         <div class="col-12 col-md-6 col-lg-4">
@@ -98,7 +100,7 @@
       <div id="register" class="row">
         <div class="col-lg-8 offset-lg-2">
           <div class="card card-shadowed p-50  mb-0" style="max-width: 70%; margin-left: 20%;">
-            <h5 class="text-uppercase text-center" id="form-head">Fill The Form</h5>
+            <h5 class="text-uppercase text-center">Fill The Form</h5>
             <form class="form-type-material" method="post" action="#" id="register_user">
               <div class="form-group">
                 <div class="input-group">
@@ -126,17 +128,23 @@
 @endsection()
 @section('footerjs')
 <script type="text/javascript">
-
-  $('#register_user').submit(function(e){
+$('#register_user').submit(function(e){
     e.preventDefault()
     value = $(this).serialize();
     response = JSON.parse(ajax_resp('/register',value))
     if(response.status == 'succ'){
       $('#form-head').html('Thank You')
-      $('#register_user').html('<i class="fa fa-check-circle" aria-hidden="true"></i><br><br><button class="btn btn-xl btn-primary" id="crowdsale" type="button" style="margin-left: 19%;">Particapte to Crowdsale</button>')
+      $('#register_user').html('<i class="fa fa-check-circle" aria-hidden="true"></i><br><br><button onclick="chaleja()" class="btn btn-xl btn-primary" id="crowdsale" type="button" style="margin-left: 19%;">Particapte to Crowdsale</button>')
     }
-  })
-/*<a href="https://crowdsale.lalaworld.io" target="_blank">*/
+  });
+
+  function chaleja(){
+    window.open(
+    'https://crowdsale.lalaworld.io',
+    '_blank'
+    );
+  }
+
   function ajax_resp(url,data){
     var tmp = null;
     $.ajax({
@@ -153,14 +161,7 @@
 
     return tmp;
   }
-  /*$('#crowdsale').click(function(e){
-    console.log("Hello")
-    window.open(
-      'https://crowsale.lalaworld.io',
-      '_blank'
-    );
-  })
-*/
+
   (function(d, s, id){
      var js, fjs = d.getElementsByTagName(s)[0];
      if (d.getElementById(id)) return;
@@ -169,14 +170,14 @@
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
 
-  //$("#share").on("click", function(e){
+  $("#share").on("click", function(e){
     /*window.open('https://www.facebook.com/dialog/share?app_id=144872242738827&%20channel_url=http://staticxx.facebook.com/connect/xd_arbiter/r/lY4eZXm_YWu.js?version=42#cb=f2ab76abba9ab74&domain=localhost&origin=http://localhost:260/f2a9377a5406c3c&relation=opener&display=popup&e2e={}&hashtag=#ICO #airdrop #eth #crypto #cryptocurrency&href=http://localhost:260&locale=pl_PL&mobile_iframe=false& next=http://staticxx.facebook.com/connect/xd_arbiter/r/lY4eZXm_YWu.js?version=42#cb=f32ed45aa21d6fc&domain=localhost&origin=http://localhost:260/f2a9377a5406c3c&relation=opener&frame=f2943a95be7814c&result="xxRESULTTOKENxx"&sdk=joey','targetWindow','toolbar=no,location=0,status=no,menubar=no,scrollbars=yes,resizable=yes,width=600,height=250'); return false*/
     /*FB.ui({
       app_id: '144872242738827',
       method: 'share',
       href: 'https://lalaworld.io',
     }, function(response){});*/
-  //})
+  })
 
   function pendingBonus(callback) {
 
@@ -227,5 +228,11 @@
     var v = $(this).find(".card-title");
     v.html("<del>" + v.html() + "</dev>");
   })
+
+  function fbShare(title, descr, image, winWidth, winHeight) {
+      var winTop = (screen.height / 2) - (winHeight / 2);
+      var winLeft = (screen.width / 2) - (winWidth / 2);
+      window.open('https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Flalaworld.io%2F&amp;src=sdkpreparse' + title + '&p[summary]=' + descr + '&p[images][0]=' + image, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + winWidth + ',height=' + winHeight);
+  }
 </script>
 @endsection()
---
+++ resources/views/mail/register.blade.php
@@ -0,0 +1,75 @@
+<table width="100%" style="background-color: #f6f6f6;width: 100%; font-family: "Helvetica Neue","Helvetica",Helvetica,Arial,sans-serif">
+<tr>
+    <td></td>
+    <td width="600" style="clear: both;display: block;margin: 0 auto; max-width: 600px; vertical-align: top;">
+        <div style="display: block;
+    margin: 0 auto;
+    max-width: 600px;
+    padding: 20px;">
+            <table  style="background: #fff none repeat scroll 0 0;
+    border: 1px solid #e9e9e9;
+    border-radius: 3px;" width="100%" cellpadding="0" cellspacing="0">
+                <tr>
+                    <td  style="padding: 20px; vertical-align: top;">
+                        <table  cellpadding="0" cellspacing="0" style="width: 550px">
+                            <tr>
+                                <td style="height: 140px; text-align: center; background: #00BA95; color:#fff"><div style="font-size: 25px;font-weight: bold"><img src="https://lalaworld.io/public/skin/images/LaLaWorld-logo-white.png" width="200"></div>
+                                    <div style="font-size: 12px">An Ecosystem for the migrants and their unbanked families</div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="padding: 0 0 20px;vertical-align: top;">
+                                    <h3 style="font-size: 18px;margin: 40px 0 0; font-weight: normal">Hello there,</h3>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="padding: 0 0 20px; font-size: 13px; line-height: 1.5; text-align: center;">
+                                    You have registered with {{ $email_address }} this email and {{ $tel_user_name }}
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td style="padding: 0 0 20px; font-size: 13px; line-height: 1.5; text-align: center;">
+                                </td>
+                            </tr>
+
+                            <tr>
+
+                            </tr>
+
+                        </table>
+                    </td>
+                </tr>
+            </table>
+            <div>
+                <table width="590">
+                    <tbody>
+                    <tr>
+                        <td style="background: #F6F6F6; color:#000; padding:20px" width="50%"><div style="font-size: 14px; font-weight:bold; padding-bottom:10px">Headquarters :</div> <span style="font-size:12px">53B, Grange Road, 18-03, Spring Grove, Singapore, 249567</span>
+
+                        </td>
+                        <td style="background:#F6F6F6; color:#000; padding:20px" width="50%">
+                            <div style="font-size: 14px; font-weight:bold; padding-bottom:10px">Support :</div>
+                            <table style="color: #000">
+                                <tbody><tr>
+                                    <td style="font-size:12px; color:#000;"><span>Call </span> @ 011-42237360</td>
+                                </tr>
+                                <tr>
+                                    <td style="font-size:12px;color:#000;">
+                                        <span>Email </span>  @ hello@lalaworld.io
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </td>
+    <td></td>
+</tr>
+</table>
+
+
